230646fa55d08f2f642b400cca68c083770e8500
Nguyen Tan Tai <tantaiIT3000@gmail.com>
2022-06-25 14:52:40 +0700

    template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,94 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World tai cho dien</h1>
+      <div class="topbar">
+        Top
+      </div>
+      <div class="appbar">
+        APp
+      </div>
+      <div class="columnbar">
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+        <div class="column">
+          <header>Tan tai</header>
+          <ul>
+            <li><img src="https://i.pinimg.com/236x/52/f2/80/52f2805d29758ed7e356ac1ed2e824bc.jpg" alt="" /></li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+            <li>What do you want to work</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
